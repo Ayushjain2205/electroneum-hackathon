14700f16a2a5b7cca3838af0864dc92ba43efb29
Ayush Jain <ayushjain2205@gmail.com>
2025-02-07 13:04:31 +0530

    Add avatar for different modes

--- components/mode-selector.tsx
+++ components/mode-selector.tsx
@@ -3,6 +3,7 @@
 import { cn } from "@/lib/utils";
 import { useMode } from "@/contexts/ModeContext";
 import { modeColors, type ModeType } from "@/lib/colors";
+import Image from "next/image";
 
 const modes: { id: ModeType; name: string }[] = [
   { id: "bff", name: "BFF" },
@@ -17,27 +18,39 @@ export function ModeSelector() {
 
   return (
     <div
-      className="w-64 border-r-2 border-black p-4 space-y-2 transition-colors duration-300 ease-in-out"
+      className="w-64 border-r-2 border-black p-4 flex flex-col justify-between h-full transition-colors duration-300 ease-in-out"
       style={{ backgroundColor: activeLightColor }}
     >
-      {modes.map((mode) => (
-        <button
-          key={mode.id}
-          onClick={() => setActiveMode(mode.id)}
-          className={cn(
-            "w-full p-3 text-left font-display font-bold border-2 border-black rounded-lg transition-all",
-            activeMode === mode.id
-              ? "text-black shadow-brutal -translate-y-0.5"
-              : "bg-white hover:bg-gray-50"
-          )}
-          style={{
-            backgroundColor:
-              activeMode === mode.id ? modeColors[mode.id].main : undefined,
-          }}
-        >
-          {mode.name}
-        </button>
-      ))}
+      <div className="space-y-2">
+        {modes.map((mode) => (
+          <button
+            key={mode.id}
+            onClick={() => setActiveMode(mode.id)}
+            className={cn(
+              "w-full p-3 text-left font-display font-bold border-2 border-black rounded-lg transition-all",
+              activeMode === mode.id
+                ? "text-black shadow-brutal -translate-y-0.5"
+                : "bg-white hover:bg-gray-50"
+            )}
+            style={{
+              backgroundColor:
+                activeMode === mode.id ? modeColors[mode.id].main : undefined,
+            }}
+          >
+            {mode.name}
+          </button>
+        ))}
+      </div>
+      <div className="mt-4 flex justify-center">
+        <div className="w-64 h-64 relative">
+          <Image
+            src={`/modes/${activeMode}.svg`}
+            alt={`${activeMode} mode`}
+            layout="fill"
+            objectFit="contain"
+          />
+        </div>
+      </div>
     </div>
   );
 }
